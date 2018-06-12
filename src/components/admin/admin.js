f36b0abac52f8a86ae8913acb24b8dd0c349163a
Archana Bansal <archanamittal0388@gmail.com>
2018-06-12 22:33:37 +0530

    Feat: making a post request to server to validate admin

--- src/components/admin/admin.js
+++ src/components/admin/admin.js
@@ -2,28 +2,49 @@ import React, {Component} from 'react'
 import {Route, Redirect} from 'react-router-dom'
 import DashBoard from './dashboard'
 import EventForm from '../eventForm'
+import config from '../../config/index'
+import http from '../../helper/http'
 
 class Admin extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      event: false
+      event: false,
+      isAuthenticated: false
     }
     this.handleEventClick = this.handleEventClick.bind(this)
+  }
+
+  componentDidMount () {
+    http.post(`${config.url}api/admin/validate`, {'a': 'b'})
+      .then((response) => {
+        if (response.status === 200) {
+          this.setState({isAuthenticated: true})
+          this.props.history.push('/admin/dashboard')
+        }
+        if (response.status === 403) {
+          this.setState({isAuthenticated: false})
+          this.props.history.push('/admin')
+        }
+      })
   }
 
   handleEventClick (event) {
     this.setState({event})
   }
   render () {
-    const {event} = this.state
+    const {event, isAuthenticated} = this.state
     return (
       <div>
-        {event && <Redirect to={`/admin/edit`} />}
-        <Route exact path='/admin' render={() => <h2> Please login as admin</h2>} />
-        <Route exact path='/admin/dashboard' render={() => <DashBoard onEventClick={this.handleEventClick} />} />
-        <Route exact path='/admin/create' component={EventForm} />
-        <Route exact path='/admin/edit' render={() => <EventForm {...event} isEditMode />} />
+        {isAuthenticated
+          ? <div>
+            <Route exact path='/admin/dashboard' render={() => <DashBoard onEventClick={this.handleEventClick} />} />
+            <Route exact path='/admin/create' component={EventForm} />
+            <Route exact path='/admin/edit' render={() => <EventForm {...event} isEditMode />} />
+            {event && <Redirect to={`/admin/edit`} />}
+          </div>
+          : <h2> Please login as admin</h2>
+        }
       </div>
     )
   }
