de244a8ab4087a15331f4300fcaa83fdd78c3ce6
Archana Bansal <archanamittal0388@gmail.com>
2018-06-21 11:20:02 +0530

    file updated

--- server/model/redis.js
+++ server/model/redis.js
@@ -17,7 +17,8 @@ const lrange = promisify(client.lrange).bind(client)
 const lset = promisify(client.lset).bind(client)
 const sadd = promisify(client.sadd).bind(client)
 const smembers = promisify(client.smembers).bind(client)
+const lpop = promisify(client.lpop).bind(client)
 
 module.exports = {
-  hmset, hget, lpush, lrange, lset, sadd, smembers
+  hmset, hget, lpush, lrange, lset, sadd, smembers, lpop
 }
